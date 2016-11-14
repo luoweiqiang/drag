93f0190715a23cee6855ef4514bbcc606dbfdbd5
luoweiqiang <392140422@qq.com>
2016-11-15 00:31:25 +0800

    change master

--- drag.js
+++ drag.js
@@ -2,6 +2,17 @@ function drag(id){
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown=function(){
-	}
-}
+	obj.onmousedown=function(ev){
+		disX = ev.pageX + obj.offsetLeft;
+		disY = ev.pageY + obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.clientX - disX + 'px';
+			obj.style.top = ev.clientY - disY + 'px';
+		};
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};
+	};
+	return false;
+};
